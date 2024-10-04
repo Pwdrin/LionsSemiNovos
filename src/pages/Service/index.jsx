ebd494c123ed95068a639d67fc355296a128d159
Legit <104234186+Pwdrin@users.noreply.github.com>
2024-10-04 18:19:23 -0300

    Update index.jsx

--- src/pages/Service/index.jsx
+++ src/pages/Service/index.jsx
@@ -8,7 +8,7 @@ function Service() {
     <>
       <Header />
       <div className="banner">
-        <img src="https://cdn.discordapp.com/attachments/843212023106109500/1290836418796458065/31553-site-aaa_copiar.png?ex=66fde898&is=66fc9718&hm=24cfbc2cd075aaaf35f9c582416c560f1cfa83814d866d9a5f4c3abdd10e3b58&" />
+        <img src="https://i.ibb.co/djj5rXM/31553-site-aaa-copiar.png" />
       </div>
       <div className="divisor">
         <GoArrowDown className="seta" />
@@ -19,7 +19,7 @@ function Service() {
             <div>
               <img
                 alt="GANHE UMA RENDA EXTRA"
-                src="https://cdn.discordapp.com/attachments/843212023106109500/1290774607284015187/icons8-dinheiro-100.png?ex=66fdaf07&is=66fc5d87&hm=e8fd62c0020acb082299cc92e660273886328d91fb5bf6ad2e17b41030460d6d&"
+                src="https://i.ibb.co/GWqZX2t/icons8-dinheiro-100.png"
               />
               <h1>FIQUE DESPREOCUPADO</h1>
             </div>
@@ -28,7 +28,7 @@ function Service() {
             <div>
               <img
                 alt="GANHE UMA RENDA EXTRA"
-                src="https://cdn.discordapp.com/attachments/843212023106109500/1290774607661371404/icons8-banho-de-sol-100.png?ex=66fdaf07&is=66fc5d87&hm=9013cc9acb4b54249005ca714972e7e19c59154b34f8b2cee6746ef38ef5f2ab&"
+                src="https://i.ibb.co/gRzQcQy/icons8-lucro-100.png"
               />
               <h1>TRABALHE DE ONDE QUISER</h1>
             </div>
@@ -37,7 +37,7 @@ function Service() {
             <div>
               <img
                 alt="LUCRO ALTO"
-                src="https://cdn.discordapp.com/attachments/843212023106109500/1290774607871217694/icons8-lucro-100.png?ex=66fdaf07&is=66fc5d87&hm=319a9330c4556cf8e12285563256a2eaf6ba9febd189cfdd1bd4511b37e302fa&"
+                src="https://i.ibb.co/52wyQ5c/icons8-banho-de-sol-100.png"
               />
               <h1>LUCRO ALTO</h1>
             </div>
@@ -56,7 +56,7 @@ function Service() {
         acompanhará cada etapa para a sua realização de sua venda.
       </h2>
       <div className="banner">
-        <img src="https://cdn.discordapp.com/attachments/843212023106109500/1290785369364762674/31553-site-aaa_copiarr.png?ex=66fdb90d&is=66fc678d&hm=a9bdb4788d4b9e6b2faf34f86a1fcad68f21f5b1382f68529b058d43801ffabb&" />
+        <img src="https://i.ibb.co/ZV7xkdm/31553-site-aaa-copiarr.png" />
       </div>
 
       <div className="divisor">
@@ -67,7 +67,7 @@ function Service() {
       </h1>
       <section>
         <div className="banner">
-          <img src="https://cdn.discordapp.com/attachments/843212023106109500/1290767582328258641/31553-site-aaa_copiar.png?ex=66fda87c&is=66fc56fc&hm=1995889f771d5b87e2c1fcb7cf53a07c60f94d1346f6c3e54620cbb369ade952&" />
+          <img src="https://i.ibb.co/bsxMCZY/31553-site-aaa-copiarrr.png" />
         </div>
         <div className="container-extrabtn">
           <button className="extra-btn">Saiba mais</button>
